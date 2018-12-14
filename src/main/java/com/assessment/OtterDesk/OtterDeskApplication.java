60f41f303b67f5f90c758341dcb7a91ad2eddf12
RashmiMuraharishetti <rashmi.muraharishetti@gmail.com>
2018-12-14 09:20:41 -0700

    first commit

---
+++ src/main/java/com/assessment/OtterDesk/OtterDeskApplication.java
@@ -0,0 +1,14 @@
+package com.assessment.OtterDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterDeskApplication.class, args);
+	}
+
+}
+
